7b224f54be156075629f596e85870e1d59bb8a5c
Rahmat Alfianto <rrahmatalfianto@gmail.com>
2021-12-02 00:13:22 +0700

    mengembalikan file yang terhapus tanpa sengaja karena ketiak tahuan

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+
+function onRequest(request, response) {
+    // console.log("request", request);
+    response.writeHead(200, {
+        "Content-Type" : "text/html",
+    });
+    fs.readFile("./index.html", null, (error, data) => {
+        if(error) {
+            response.writeHead(404);
+            response.write("file not found");
+        } else {
+            response.write(data)
+        }
+        response.end()
+    })
+}
+
+http.createServer(onRequest).listen(4000)
